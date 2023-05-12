578039500e9ff4f7f4e5509fe2ff2d14d508f193
Mjey21 <120388398+Mjey21@users.noreply.github.com>
2023-05-12 12:47:29 +0300

    Create s21_bits.c

---
+++ src/s21_bits.c
@@ -0,0 +1,23 @@
+int s21_get_bit_big(s21_big_decimal src, int index){
+}
+
+void s21_set_bit_big(s21_big_decimal *dst, int index, int bit){
+}
+
+int s21_get_bit(s21_decimal src, int index){
+}
+
+int s21_get_scale(s21_decimal dst){
+}
+
+int s21_get_sign(s21_decimal dst){
+}
+
+void s21_set_bit(s21_decimal *dst, int index, int bit){
+}
+
+void s21_set_scale(s21_decimal *dst, int scale){
+}
+
+void s21_set_sign(s21_decimal *dst){
+}
